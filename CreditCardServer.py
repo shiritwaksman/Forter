abc6394ee4723cea9bc43855343b5cf518e37141
Shirit <shiritvv@gmail.com>
2016-11-10 12:24:51 +0200

    basic get and put without edge cases

--- CreditCardServer.py
+++ CreditCardServer.py
@@ -1,18 +1,30 @@
 import tornado.ioloop
 import tornado.web
+import uuid
 
 class CreditCardHandler(tornado.web.RequestHandler):
 
-    def get(self):
-        self.write("get")
+    def initialize(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def get(self, creditcard):
+        """retrieving a stored credit card"""
+        self.write('{"credit-card":"' + self.tokenizer[creditcard] + '"}')
 
     def post(self):
-        self.write("post")
+        """"inserting a new credit card"""
+        creditcard = str(tornado.escape.json_decode(self.request.body)["credit-card"])
+        token = str(uuid.uuid3(uuid.NAMESPACE_DNS, creditcard))
+        self.tokenizer[token] = creditcard
+        # The response is a unique id that represents the credit card token
+        self.write('{"token" : "' + token + '"}')
+
 
 def make_app():
     tokenize_credit_cards = {}
     return tornado.web.Application([
-        (r"/creditcard", CreditCardHandler)
+        (r"/creditcard/([^/]+)", CreditCardHandler, dict(tokenizer=tokenize_credit_cards)),
+        (r"/creditcard", CreditCardHandler, dict(tokenizer=tokenize_credit_cards))
     ])
 
 def main():
